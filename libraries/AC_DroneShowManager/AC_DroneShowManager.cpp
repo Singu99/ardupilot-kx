e95825e00c154d07d4f5e3d7ed6012ab6a4b4151
Singu99 <marc.espuna@salle.url.edu>
2024-09-11 20:47:44 +0200

    Fixed collmot directory to lowercase since littlefs is case sensitive

--- libraries/AC_DroneShowManager/AC_DroneShowManager.cpp
+++ libraries/AC_DroneShowManager/AC_DroneShowManager.cpp
@@ -22,6 +22,54 @@
 #include "DroneShowLEDFactory.h"
 #include "DroneShowPyroDeviceFactory.h"
 
+#ifndef HAL_BOARD_COLLMOT_DIRECTORY
+#  if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+#    define HAL_BOARD_COLLMOT_DIRECTORY "./collmot"
+#  else
+#    define HAL_BOARD_COLLMOT_DIRECTORY "/collmot"
+#  endif
+#endif
+
+#define SHOW_FILE (HAL_BOARD_COLLMOT_DIRECTORY "/show.skyb")
+
+// Default update rate for position and velocity targets
+#define DEFAULT_UPDATE_RATE_HZ 10
+
+// Length of a GPS week in seconds
+#define GPS_WEEK_LENGTH_SEC 604800
+
+// Length of a GPS week in milliseconds
+#define GPS_WEEK_LENGTH_MSEC 604800000
+
+// Smallest valid value of show AMSL. Values smaller than this are considered unset.
+#define SMALLEST_VALID_AMSL -9999999
+
+// Largest valid value of show AMSL. Values larger than this are considered invalid.
+#define LARGEST_VALID_AMSL 10000000
+
+// Default altitude to take off to when starting the show, in meters. The drone
+// will take off to this altitude above its current position.
+#define DEFAULT_TAKEOFF_ALTITUDE_METERS 2.5f
+
+// Default time synchronization mode
+#define DEFAULT_SYNC_MODE TimeSyncMode_GPS
+
+// Default takeoff placement error tolerance level, in meters. The drone will not
+// take off if it is placed farther than this distance from its takeoff position.
+#define DEFAULT_XY_PLACEMENT_ERROR_METERS 3.0f
+
+// Default horizontal trajectory drift tolerance level, in meters.
+#define DEFAULT_MAX_XY_DRIFT_METERS 3.0f
+
+// Default vertical trajectory drift tolerance level, in meters.
+#define DEFAULT_MAX_Z_DRIFT_METERS 3.0f
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+// UDP port that the drone show manager uses to broadcast the status of the RGB light
+// when compiled with the SITL simulator. Uncomment if you need it.
+// #  define RGB_SOCKET_PORT 4245
+#endif
+
 extern const AP_HAL::HAL &hal;
 
 // LED factory that is used to create new RGB LED instances
